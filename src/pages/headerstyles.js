9caf7b6f15982cca0c004bac7a779622fc3303d8
kibokusut <kibokusut@gmail.com>
2021-02-05 16:49:01 +0700

    jdf

---
+++ src/pages/headerstyles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Wrap = styled.header`
+  background-color: #292961;
+  min-height: 40px;
+  height: 40px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  padding: 0 16px;
+`;
